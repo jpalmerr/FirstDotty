b278ecc36815cec1c7c9fd371c426ddd651a5925
James Palmer <james.palmer@itv.com>
2020-10-26 18:41:03 +0000

    First dotty, types

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+    println(msg)
+    
+    runExample("Intersection Types")(IntersectionTypes.test)
+    runExample("Union Types")(UnionTypes.test)
+  }
+
+  def msg = "I was compiled by dotty :)"
+
+  private def runExample(name: String)(f: => Unit) = {
+    println(s"$name example:")
+    f
+    println()
+  }
+
+}
---
+++ src/main/scala/UnionTypes.scala
@@ -0,0 +1,19 @@
+object UnionTypes {
+
+  case class UserName(name: String)
+  case class UserNumber(number: Int)
+  
+  def help(userId: UserName | UserNumber) = {
+    val user = userId match {
+      case UserName(name) => println(s"name: ${name}")
+      case UserNumber(number) => println(s"number: ${number}")
+    }
+  }
+  
+  def test: Unit = {
+    val userName = UserName("name")
+    val userNumber = UserNumber(12345)
+    help(userName)
+    help(userNumber) 
+  }
+}
